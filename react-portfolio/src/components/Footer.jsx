f5234c796404b2a69197c63f256e567e631df6bb
langiam <ryan.matthew.lang@gmail.com>
2025-03-19 17:03:08 -0400

    added footer and final app code

---
+++ react-portfolio/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Connect with me:</p>
+      <a href="https://github.com/langiam">GitHub</a> |
+      <a href="www.linkedin.com/in/ryan-lang-610a0723">LinkedIn</a> |
+    </footer>
+  );
+};
+
+export default Footer;
